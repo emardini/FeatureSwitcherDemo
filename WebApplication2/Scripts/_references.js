b8e74a1ee7d519ec2a2366b7842f039cf01ad7bf
Eduardo Mardini <emardini@outsitenetworks.com>
2019-03-19 10:32:13 -0400

    Update nuget packages

--- WebApplication2/Scripts/_references.js
+++ WebApplication2/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <autosync enabled="true" />
+﻿/// <reference path="jquery-3.3.1.js" />
+/// <autosync enabled="true" />
 /// <reference path="modernizr-2.6.2.js" />
-/// <reference path="jquery-1.10.2.js" />
 /// <reference path="bootstrap.js" />
 /// <reference path="respond.js" />
